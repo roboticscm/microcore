36ebc56a4db919ed969838ef989fcfb2237e9aa5
khai.lv <khai.lv@suntech.com.vn>
2022-03-26 05:25:56 +0700

    ...

--- web/src/config/server.js
+++ web/src/config/server.js
@@ -2,8 +2,8 @@ import { dev } from '$app/env';
 
 export const config = dev ? {
     serverPort: 3000,
-    messagingServer: 'http://localhost:3002',
+    messagingServer: 'http://18.140.147.176:3002',
 } : {
     serverPort: 3000,
-    messagingServer: 'http://localhost:3002',
+    messagingServer: 'http://18.140.147.176:3002',
 }
